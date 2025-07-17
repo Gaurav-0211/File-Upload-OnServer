7731318a41419830608b55c495b0465edf21bdcd
Gaurav Kumar <gauravkr0211@gmail.com>
2025-07-17 12:21:17 +0530

    Static Implemented

---
+++ src/main/java/com/file/controller/FileUploadController.java
@@ -0,0 +1,44 @@
+package com.file.controller;
+
+import com.file.FileUploadApplication;
+import com.file.helper.FileUploadHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/file")
+public class FileUploadController {
+    @Autowired
+    private FileUploadHelper fileUploadHelper;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> createFile(@RequestParam ("file")MultipartFile file){
+//        System.out.println(file.getOriginalFilename());
+//        System.out.println(file.getSize());
+//        System.out.println(file.getContentType());
+//        System.out.println(file.getName());
+
+        try {
+            if (file.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Request must contain a file");
+            }
+
+            if(!file.getContentType().equals("image/jpeg")){
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Only JPEG file type allowed");
+            }
+
+            boolean f = fileUploadHelper.uploadFile(file);
+            if(f){
+                return ResponseEntity.ok("File Successfully uploaded");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong");
+    }
+}
---
+++ src/main/java/com/file/helper/FileUploadHelper.java
@@ -0,0 +1,42 @@
+package com.file.helper;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Component
+public class FileUploadHelper {
+
+    public final String UPLOAD_DIR="D:\\Training_Project\\File-Upload\\src\\main\\resources\\static\\file";
+
+    public boolean uploadFile(MultipartFile file){
+        boolean f = false;
+        try {
+//            InputStream is = file.getInputStream();
+//            byte[] data = new byte[is.available()];
+//
+//
+//            is.read(data);
+//
+//            FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+ File.separator+file.getOriginalFilename());
+//            fos.write(data);
+//            fos.flush();
+//            fos.close();
+
+            Files.copy(file.getInputStream(),Paths.get(UPLOAD_DIR+File.separator+file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+            f = true;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return f;
+    }
+}
